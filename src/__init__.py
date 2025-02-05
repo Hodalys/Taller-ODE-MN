43ac9347ad967b6280ea6c274d03f7b1d6f6eb51
Hodalys <hodalys.lopez@epn.edu.ec>
2025-02-05 09:52:07 -0500

    :bookmark: ODE ejercicios

--- src/__init__.py
+++ src/__init__.py
@@ -8,8 +8,4 @@ from .linear_sist_methods import (
 
 from .iterative_methods import gauss_jacobi, gauss_seidel  # type: ignore
 
-<<<<<<< HEAD
 from .ODE import ODE_euler, ODE_euler_nth  # type: ignore
-=======
-from .ODE import ODE_euler, ODE_euler_nth  # type: ignore
->>>>>>> 54e308131f38cae39e6c6775cf95b757e349b9ae
